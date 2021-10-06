cd385c7d0e78f1f3c57aad1f29283a1d69dd18a3
prixrak <20arman15@gmail.com>
2021-10-06 16:34:07 +0300

    fix problem with port in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const __dirname = path.resolve();
 // get PORT from constants
 let PORT = config.get("port") || 3000;
 if (process.env.NODE_ENV === "production") {
-  const PORT = process.env.PORT || 3000;
+  PORT = process.env.PORT || 3000;
 }
 
 const app = express(); 
